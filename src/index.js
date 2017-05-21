3a21e243904ae4f46e958428461f61db978b8264
Pelirrojo <meusebio.depaz@bbva.com>
2017-05-21 21:22:54 +0200

    Adding utils

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+/* global moment:false */
+/* global jmespath:false */
+/* global _:false */
+
 angular
-  .module('app', ['ui.router', 'ui.bootstrap']);
+  .module('app', ['ui.router', 'ui.bootstrap'])
+  .constant('moment', moment)
+  .constant('jmespath', jmespath)
+  .constant('_', _);
