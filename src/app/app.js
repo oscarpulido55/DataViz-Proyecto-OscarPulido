bdda9a82382e07bde0b2135541df40a8662ce003
Oscar Pulido <oscarpulido55@hotmail.com>
2017-05-29 19:45:47 +0200

    media torta

--- src/app/app.js
+++ src/app/app.js
@@ -55,218 +55,282 @@ angular
         values: rawData
       }]
 
-        //GRafico 2
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////7
+      //GRafico 2
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////7
 
-        this.chart1 = {data:{id:1},options:{}}
+      this.chart1 = {data:{id:1},options:{}}
 
-        this.chart1.options = {
-            chart: {
-                type: 'lineWithFocusChart',
-                height: 450,
-                margin : {
-                    top: 20,
-                    right: 20,
-                    bottom: 60,
-                    left: 40
-                },
-                duration: 500,
-                useInteractiveGuideline: true,
-                xAxis: {
-                    axisLabel: 'X Axis',
-                    tickFormat: function(d){
-                        return d3.format(',f')(d);
-                    }
-                },
-                x2Axis: {
-                    tickFormat: function(d){
-                        return d3.format(',f')(d);
-                    }
-                },
-                yAxis: {
-                    axisLabel: 'Y Axis',
-                    tickFormat: function(d){
-                        return d3.format(',.2f')(d);
-                    },
-                    rotateYLabel: false
-                },
-                y2Axis: {
-                    tickFormat: function(d){
-                        return d3.format(',.2f')(d);
-                    }
-                }
-
+      this.chart1.options = {
+        chart: {
+          type: 'lineWithFocusChart',
+          height: 450,
+          margin : {
+            top: 20,
+            right: 20,
+            bottom: 60,
+            left: 40
+          },
+          duration: 500,
+          useInteractiveGuideline: true,
+          xAxis: {
+            axisLabel: 'X Axis',
+            tickFormat: function(d){
+              return d3.format(',f')(d);
             }
-        };
-
-                this.chart1.data = generateData();
-
-        /* Random Data Generator (took from nvd3.org) */
-        function generateData() {
-            return stream_layers(3,10+Math.random()*200,.1).map(function(data, i) {
-                return {
-                    key: 'Stream' + i,
-                    values: data
-                };
-            });
-        }
-
-        /* Inspired by Lee Byron's test data generator. */
-        function stream_layers(n, m, o) {
-            if (arguments.length < 3) o = 0;
-            function bump(a) {
-                var x = 1 / (.1 + Math.random()),
-                    y = 2 * Math.random() - .5,
-                    z = 10 / (.1 + Math.random());
-                for (var i = 0; i < m; i++) {
-                    var w = (i / m - y) * z;
-                    a[i] += x * Math.exp(-w * w);
-                }
+          },
+          x2Axis: {
+            tickFormat: function(d){
+              return d3.format(',f')(d);
             }
-            return d3.range(n).map(function() {
-                var a = [], i;
-                for (i = 0; i < m; i++) a[i] = o + o * Math.random();
-                for (i = 0; i < 5; i++) bump(a);
-                return a.map(stream_index);
-            });
-        }
+          },
+          yAxis: {
+            axisLabel: 'Y Axis',
+            tickFormat: function(d){
+              return d3.format(',.2f')(d);
+            },
+            rotateYLabel: false
+          },
+          y2Axis: {
+            tickFormat: function(d){
+              return d3.format(',.2f')(d);
+            }
+          }
 
-        /* Another layer generator using gamma distributions. */
-        function stream_waves(n, m) {
-            return d3.range(n).map(function(i) {
-                return d3.range(m).map(function(j) {
-                    var x = 20 * j / m - i / 3;
-                    return 2 * x * Math.exp(-.5 * x);
-                }).map(stream_index);
-            });
         }
+      };
 
-        function stream_index(d, i) {
-            return {x: i, y: Math.max(0, d)};
+      this.chart1.data = generateData();
+
+      /* Random Data Generator (took from nvd3.org) */
+      function generateData() {
+        return stream_layers(3,10+Math.random()*200,.1).map(function(data, i) {
+          return {
+            key: 'Stream' + i,
+            values: data
+          };
+        });
+      }
+
+      /* Inspired by Lee Byron's test data generator. */
+      function stream_layers(n, m, o) {
+        if (arguments.length < 3) o = 0;
+        function bump(a) {
+          var x = 1 / (.1 + Math.random()),
+            y = 2 * Math.random() - .5,
+            z = 10 / (.1 + Math.random());
+          for (var i = 0; i < m; i++) {
+            var w = (i / m - y) * z;
+            a[i] += x * Math.exp(-w * w);
+          }
         }
+        return d3.range(n).map(function() {
+          var a = [], i;
+          for (i = 0; i < m; i++) a[i] = o + o * Math.random();
+          for (i = 0; i < 5; i++) bump(a);
+          return a.map(stream_index);
+        });
+      }
+
+      /* Another layer generator using gamma distributions. */
+      function stream_waves(n, m) {
+        return d3.range(n).map(function(i) {
+          return d3.range(m).map(function(j) {
+            var x = 20 * j / m - i / 3;
+            return 2 * x * Math.exp(-.5 * x);
+          }).map(stream_index);
+        });
+      }
+
+      function stream_index(d, i) {
+        return {x: i, y: Math.max(0, d)};
+      }
 
 //GRafico 3
-        ///////////////////////////////////////////////////////////////////////////////////////////////////////////////7
-        this.chart3 = {
-          data: {
-            id:1
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////7
+      this.chart3 = {
+        data: {
+          id:1
+        },
+        options:{}
+      }
+
+      this.chart3.options = {
+        chart: {
+          type: 'multiBarHorizontalChart',
+          height: 450,
+          x: function(d){return d.label;},
+          y: function(d){return d.value;},
+          //yErr: function(d){ return [-Math.abs(d.value * Math.random() * 0.3), Math.abs(d.value * Math.random() * 0.3)] },
+          showControls: true,
+          showValues: true,
+          duration: 500,
+          xAxis: {
+            showMaxMin: false
           },
-          options:{}
-        }
-
-        this.chart3.options = {
-            chart: {
-                type: 'multiBarHorizontalChart',
-                height: 450,
-                x: function(d){return d.label;},
-                y: function(d){return d.value;},
-                //yErr: function(d){ return [-Math.abs(d.value * Math.random() * 0.3), Math.abs(d.value * Math.random() * 0.3)] },
-                showControls: true,
-                showValues: true,
-                duration: 500,
-                xAxis: {
-                    showMaxMin: false
-                },
-                yAxis: {
-                    axisLabel: 'Values',
-                    tickFormat: function(d){
-                        return d3.format(',.2f')(d);
-                    }
-                }
+          yAxis: {
+            axisLabel: 'Values',
+            tickFormat: function(d){
+              return d3.format(',.2f')(d);
             }
-        };
+          }
+        }
+      };
 
-                this.chart3.data=
-                [
-                    {
-                        "key": "Series1",
-                        "color": "#d62728",
-                        "values": [
-                            {
-                                "label" : "Group A" ,
-                                "value" : -1.8746444827653
-                            } ,
-                            {
-                                "label" : "Group B" ,
-                                "value" : -8.0961543492239
-                            } ,
-                            {
-                                "label" : "Group C" ,
-                                "value" : -0.57072943117674
-                            } ,
-                            {
-                                "label" : "Group D" ,
-                                "value" : -2.4174010336624
-                            } ,
-                            {
-                                "label" : "Group E" ,
-                                "value" : -0.72009071426284
-                            } ,
-                            {
-                                "label" : "Group F" ,
-                                "value" : -0.77154485523777
-                            } ,
-                            {
-                                "label" : "Group G" ,
-                                "value" : -0.90152097798131
-                            } ,
-                            {
-                                "label" : "Group H" ,
-                                "value" : -0.91445417330854
-                            } ,
-                            {
-                                "label" : "Group I" ,
-                                "value" : -0.055746319141851
-                            }
-                        ]
-                    },
-                    {
-                        "key": "Series2",
-                        "color": "#1f77b4",
-                        "values": [
-                            {
-                                "label" : "Group A" ,
-                                "value" : 25.307646510375
-                            } ,
-                            {
-                                "label" : "Group B" ,
-                                "value" : 16.756779544553
-                            } ,
-                            {
-                                "label" : "Group C" ,
-                                "value" : 18.451534877007
-                            } ,
-                            {
-                                "label" : "Group D" ,
-                                "value" : 8.6142352811805
-                            } ,
-                            {
-                                "label" : "Group E" ,
-                                "value" : 7.8082472075876
-                            } ,
-                            {
-                                "label" : "Group F" ,
-                                "value" : 5.259101026956
-                            } ,
-                            {
-                                "label" : "Group G" ,
-                                "value" : 0.30947953487127
-                            } ,
-                            {
-                                "label" : "Group H" ,
-                                "value" : 0
-                            } ,
-                            {
-                                "label" : "Group I" ,
-                                "value" : 0
-                            }
-                        ]
-                    }
-                ]
+      this.chart3.data=
+        [
+          {
+            "key": "Series1",
+            "color": "#d62728",
+            "values": [
+              {
+                "label" : "Group A" ,
+                "value" : -1.8746444827653
+              } ,
+              {
+                "label" : "Group B" ,
+                "value" : -8.0961543492239
+              } ,
+              {
+                "label" : "Group C" ,
+                "value" : -0.57072943117674
+              } ,
+              {
+                "label" : "Group D" ,
+                "value" : -2.4174010336624
+              } ,
+              {
+                "label" : "Group E" ,
+                "value" : -0.72009071426284
+              } ,
+              {
+                "label" : "Group F" ,
+                "value" : -0.77154485523777
+              } ,
+              {
+                "label" : "Group G" ,
+                "value" : -0.90152097798131
+              } ,
+              {
+                "label" : "Group H" ,
+                "value" : -0.91445417330854
+              } ,
+              {
+                "label" : "Group I" ,
+                "value" : -0.055746319141851
+              }
+            ]
+          },
+          {
+            "key": "Series2",
+            "color": "#1f77b4",
+            "values": [
+              {
+                "label" : "Group A" ,
+                "value" : 25.307646510375
+              } ,
+              {
+                "label" : "Group B" ,
+                "value" : 16.756779544553
+              } ,
+              {
+                "label" : "Group C" ,
+                "value" : 18.451534877007
+              } ,
+              {
+                "label" : "Group D" ,
+                "value" : 8.6142352811805
+              } ,
+              {
+                "label" : "Group E" ,
+                "value" : 7.8082472075876
+              } ,
+              {
+                "label" : "Group F" ,
+                "value" : 5.259101026956
+              } ,
+              {
+                "label" : "Group G" ,
+                "value" : 0.30947953487127
+              } ,
+              {
+                "label" : "Group H" ,
+                "value" : 0
+              } ,
+              {
+                "label" : "Group I" ,
+                "value" : 0
+              }
+            ]
+          }
+        ]
+
+      //GRafico 4
+      ///////////////////////////////////////////////////////////////////////////////////////////////////////////////7
+
+      this.chart4 = {
+        data: {
+          id:1
+        },
+        options:{}
+      };
+
+      this.chart4.options = {
+        chart: {
+          type: 'pieChart',
+          height: 450,
+          donut: true,
+          x: function(d){return d.key;},
+          y: function(d){return d.y;},
+          showLabels: true,
+
+          pie: {
+            startAngle: function(d) {return d.startAngle/2 -Math.PI/2 },
+            endAngle: function(d) {return d.endAngle/2 -Math.PI/2 }
+          },
+          duration: 500,
+          legend: {
+            margin: {
+              top: 5,
+              right: 140,
+              bottom: 5,
+              left: 0
+            }
+          }
+        }
+      };
 
 
+      this.chart4.data= [
+        {
+          key: "One",
+          y: 5
+        },
+        {
+          key: "Two",
+          y: 2
+        },
+        {
+          key: "Three",
+          y: 9
+        },
+        {
+          key: "Four",
+          y: 7
+        },
+        {
+          key: "Five",
+          y: 4
+        },
+        {
+          key: "Six",
+          y: 3
+        },
+        {
+          key: "Seven",
+          y: .5
+        }
+      ];
 
 
-    
     }
   });
